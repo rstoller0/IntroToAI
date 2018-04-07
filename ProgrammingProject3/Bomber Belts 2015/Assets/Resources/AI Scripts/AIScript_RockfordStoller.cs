5481f685aad2e855dd1cd6a8a091511641e051da
rstoller0 <rockfordstoller@gmail.com>
2018-04-07 00:48:34 -0400

    Shit Start

--- ProgrammingProject3/Bomber Belts 2015/Assets/Resources/AI Scripts/AIScript_RockfordStoller.cs
+++ ProgrammingProject3/Bomber Belts 2015/Assets/Resources/AI Scripts/AIScript_RockfordStoller.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class AI_Template : MonoBehaviour {
+public class AIScript_RockfordStoller : MonoBehaviour {
 
     public CharacterScript mainScript;
 
@@ -10,9 +11,12 @@ public class AI_Template : MonoBehaviour {
     public float playerSpeed;
     public int[] beltDirections;
     public float[] buttonLocations;
+    //added
+    public float[] bombDistances;
+    int targetButton = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         mainScript = GetComponent<CharacterScript>();
 
         if (mainScript == null)
@@ -33,7 +37,110 @@ public class AI_Template : MonoBehaviour {
         beltDirections = mainScript.getBeltDirections();
 
 
-        
+
         //Your AI code goes here
-	}
+
+        //how fast the bombs move
+        bombSpeeds = mainScript.getBombSpeeds();
+        //how far the bomb is from exploding
+        bombDistances = mainScript.getBombDistances();
+
+        //variables
+        int currentIndex = 0;
+        float currentDistance = int.MaxValue;
+        List<int> PossibleIndexes = new List<int>();
+
+        for (int i = 0; i < beltDirections.Length; i++) {
+            if (Mathf.Abs(mainScript.getCharacterLocation() - buttonLocations[i]) < currentDistance) {
+                currentIndex = i;
+                currentDistance = Mathf.Abs(mainScript.getCharacterLocation() - buttonLocations[i]);
+            }
+
+            if (beltDirections[i] == -1 || beltDirections[i] == 0) {
+                float bombTime = bombDistances[i] / bombSpeeds[i];
+                float playerTime = Mathf.Abs(mainScript.getCharacterLocation() - buttonLocations[i]) / playerSpeed;
+
+                if (playerTime < bombTime && bombTime > buttonCooldowns[i]) {
+                    PossibleIndexes.Add(i);
+                }
+            }
+        }
+
+        float currentTime = Mathf.Infinity;
+        foreach (int i in PossibleIndexes)
+        {
+            float bombTime = bombDistances[i] / bombSpeeds[i];
+
+            if (currentTime > bombTime)
+            {
+                currentTime = bombTime;
+                targetButton = i;
+            }
+        }
+
+        if (buttonLocations[targetButton] < mainScript.getCharacterLocation()) {
+            mainScript.moveDown();
+        }
+        else if (buttonLocations[targetButton] > mainScript.getCharacterLocation()) {
+            mainScript.moveUp();
+        }
+
+        bool canMakeIt = (Mathf.Abs(mainScript.getCharacterLocation() - buttonLocations[targetButton]) / playerSpeed) + 0.35f < bombDistances[targetButton] / bombSpeeds[targetButton];
+        bool onTarget = targetButton == currentIndex;
+
+        if (beltDirections[currentIndex] != 1) {
+            if (canMakeIt || onTarget) {
+                mainScript.push();
+            }
+        }
+
+
+        //simple backup code
+        #region
+        /*
+        //variables for determining moves
+        float minDistance = int.MaxValue;
+        int minIndex = 0;
+        float currentDistance; 
+        
+        //first attempt
+        for (int i = 0; i < beltDirections.Length; i++) {//find the closest usabe button
+            currentDistance = Mathf.Abs(buttonLocations[i] - mainScript.getCharacterLocation());
+            if (buttonCooldowns[i] <= 0 && (beltDirections[i] == -1 || beltDirections[i] == 0)) {
+                if (currentDistance < minDistance) {
+                    minIndex = i;
+                    minDistance = currentDistance;
+                } else if(beltDirections[i] < beltDirections[minIndex]) {
+                    minIndex = i;
+                    minDistance = currentDistance;
+                }
+            }
+        }
+
+        //second attempt
+        for (int i = 0; i < beltDirections.Length; i++) {//find the closest usabe button
+            currentDistance = Mathf.Abs(buttonLocations[i] - mainScript.getCharacterLocation());
+            if (buttonCooldowns[i] <= 0 && (beltDirections[i] == -1 || beltDirections[i] == 0)) {
+                if (currentDistance < minDistance && beltDirections[i] <= beltDirections[minIndex]) {
+                    minIndex = i;
+                    minDistance = currentDistance;
+                } else if (beltDirections[i] < beltDirections[minIndex]) {
+                    minIndex = i;
+                    minDistance = currentDistance;
+                }
+            }
+        }
+
+        int targetButtonIndex = minIndex;
+
+        if(buttonLocations[targetButtonIndex] < mainScript.getCharacterLocation()) {
+            mainScript.moveDown();
+            mainScript.push();
+        } else {
+            mainScript.moveUp();
+            mainScript.push();
+        }
+        */
+        #endregion
+    }
 }
